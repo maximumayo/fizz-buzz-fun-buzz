99d7068444937164b3f16dabfba040b84cdd1c13
maximumayo <maxgmayo@gmail.com>
2016-04-12 15:39:29 -0700

    #2 buttons for display and clear

--- buzz.js
+++ buzz.js
@@ -1,20 +1,32 @@
-function fizzBuzz() {
+function fizzBuzzer() {
 
     for (var i = 1; i <= 100; i++) {
-        //console.log(i);
+
         if (i % 3 === 0 && i % 5 === 0) {
+            $('#display').append("<div class='fizzbuzz'>fizzbuzz</div>");
             console.log("fizzbuzz");
         }
         else if (i % 5 === 0) {
+            $('#display').append("<div class='buzz'>buzz</div>");
             console.log("buzz");
         }
         else if (i % 3 === 0) {
+            $('#display').append("<div class='fizz'>fizz</div>");
             console.log("fizz");
         }
         else {
+            $('#display').append("<div>" + i + "</div>");
             console.log(i);
         }
     }
 }
 
-fizzBuzz();
+$(document).ready(function () {
+    $('#buzzer').click(function(){
+        fizzBuzzer();
+    });
+    $('#clear').click(function(){
+        $('#display').html(" ");
+    });
+
+});
